5fb9a2b1b0dc6123c77256c98b4f8f63f17a3510
AlbertKoii <a666523104@gmail.com>
2024-11-12 17:54:34 +0800

    測試3 merge home page
    測試3讓github 顯示出來

---
+++ .history/vite.config_20241112175104.ts
@@ -0,0 +1,18 @@
+import path, { join } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+export default defineConfig({
+  plugins: [react() , svgr()],
+  base: {}
+  server: {
+    host: '0.0.0.0', // 允许局域网访问
+    port: 5000
+},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
